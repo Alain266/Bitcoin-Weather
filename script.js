b1b0e47e7d559ee81eb6100b0efdd930392f8ca7
Alain266 <a.okala26@gmail.com>
2023-08-18 00:29:47 +0200

    responsive bug menu hamburger

--- script.js
+++ script.js
@@ -183,24 +183,25 @@ TxtRotate.prototype.tick = function() {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = `<span class="wrap">${this.txt}</span>`;
 
-  var that = this;
   var delta = 300 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) {
+    delta /= 2;
+  }
 
   if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
     this.isDeleting = true;
+    delta = this.period;
   } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
   }
 
-  setTimeout(function() {
-    that.tick();
+  setTimeout(() => {
+    this.tick();
   }, delta);
 };
 
